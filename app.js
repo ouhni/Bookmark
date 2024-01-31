2a193dfa8156124f75823a5d4907320907d8941c
Yassine <youhni.123@gmail.com>
2024-01-31 02:48:13 -0500

    Delete Bookmarks from both UI & Local Storage

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const bookmarkBtn = document.querySelector('#bookmark_btn');
 // console.log(bookmarkBtn);
 const bookmarkList = document.querySelector('.bookmarks');
 // console.log(bookmarkList);
+
+document.addEventListener('DOMContentLoaded', getBookmarks);
+
+// Get bookmarks from local storage
+function getBookmarks() {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    if(!bookmarks) {
+        bookmarks = [];
+    }
+
+    bookmarks.forEach(bookmark => {
+        const markup = `<div class="bookmark-card">
+                    <p>${bookmark.text} <span id="bookmark_id">${bookmark.id}</span></p>
+                    <p id="date">Added On: ${bookmark.date} </p>
+                    <a href="${bookmark.url}" class="btn btn-primary" target="_black">View</a>
+                    <a href="#" id="delete-bookmark" class="btn btn-danger">Delete</a>
+                </div>`;
+
+                bookmarkList.insertAdjacentHTML('afterbegin', markup)        
+    });
+}
+
 const date = new Date(Date.now()).toString().substring(0, 24);
 
 
@@ -51,3 +74,33 @@ function addBookmark() {
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
 });
+
+// Delete Bookmark from list
+bookmarkList.addEventListener('click', e => {
+
+    if(e.target.matches('#delete-bookmark')) {
+        let id = document.querySelector('#bookmark_id').textContent;
+        e.target.parentElement.remove();
+
+        removeBookmarkFromLocalStorage(id);
+    }
+})
+
+// Delete Bookmark from local storage
+function removeBookmarkFromLocalStorage(id) {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    if(!bookmarks) {
+        bookmarks = [];
+    } 
+
+    bookmarks.forEach((bookmark, index) => {
+        if(bookmark.id === parseInt(id)) {
+            bookmarks.splice(index, 1);
+        }
+    })
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+}
+
